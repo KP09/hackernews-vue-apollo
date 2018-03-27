4ebf875e030cf54751279257423155ed4114172d
Kees Postma <kk.postma@gmail.com>
2018-03-27 23:57:04 +0200

    getting started

---
+++ src/main.js
@@ -0,0 +1,47 @@
+// 1
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import 'tachyons'
+import Vue from 'vue'
+
+// 2
+import VueApollo from 'vue-apollo'
+
+import App from './App'
+import router from './router'
+
+Vue.config.productionTip = false
+
+// 3
+const httpLink = new HttpLink({
+  // You should use an absolute URL here
+  uri: 'https://api.graph.cool/simple/v1/cjfa6m4xj6gt1014372wl7rtt'
+})
+
+// 4
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+// 5
+Vue.use(VueApollo)
+
+// 6
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  // 7
+  provide: apolloProvider.provide(),
+  router,
+  render: h => h(App)
+})
